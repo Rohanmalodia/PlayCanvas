98e7d31c4c55c640e9618b20b3f876385cced50a
Rohanmalodia <55531074+Rohanmalodia@users.noreply.github.com>
2021-08-21 18:46:48 +0530

    Add files via upload

---
+++ PlayCanvas/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1216725.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 53950159, 53950160, 53950161, 53950162, 53951936 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
